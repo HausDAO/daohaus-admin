830482ef35f395336253ba53a20103924ba9b334
Prism Codex <prism-codex@users.noreply.github.com>
2026-04-22 15:32:53 +0000

    Add temporary prism staging text to hub

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,20 @@
 import { useAccount } from 'wagmi';
+import styled from 'styled-components';
 import { HomeDashboard } from '@/components/HomeDashboard';
 import { HomeNotConnected } from '@/components/HomeNotConnected';
+import { ParMd } from '@/lib/ui';
+
+const StagingNote = styled(ParMd)`
+  margin: 0 0 2.4rem;
+  font-weight: 700;
+`;
 
 export const Home = () => {
   const { isConnected } = useAccount();
-  return isConnected ? <HomeDashboard /> : <HomeNotConnected />;
+  return (
+    <>
+      <StagingNote>this is temporary staging for prism development</StagingNote>
+      {isConnected ? <HomeDashboard /> : <HomeNotConnected />}
+    </>
+  );
 };
